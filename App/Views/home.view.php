49280a3b97dead95a179eb48eba3a233733a5724
Prash <prash.somaiya@gmail.com>
2015-12-27 22:52:43 +0000

    homepage update

--- App/Views/home.view.php
+++ App/Views/home.view.php
@@ -4,7 +4,7 @@
         <h1>DVD Rental Website</h1>
     </div>
     <p class="lead">
-        Welcome to the DVD Rental Website. You can browse our selection of DVDs and rent them, prices starting from just £1.00! You can view our featured DVDs below, or, <a href="<?php echo l('browse'); ?>">click here</a> to view our entire range.
+        Welcome to the DVD Rental Website. You can browse our selection of DVDs and rent them, prices starting from just £1.00! You can view <a href="<?php echo l('browse/search'); ?>">search for DVDs</a>, or, <a href="<?php echo l('browse'); ?>">click here</a> to view our entire range.
     </p>
 </div>
 <?php include view('layout.footer'); ?>
